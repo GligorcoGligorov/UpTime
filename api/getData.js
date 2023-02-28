51239f8ffd9d971c3266816cd6872790ca4431a4
unknown <ggligorov1709@yahoo.com>
2023-02-28 02:30:49 +0100

    Add lambda function

--- api/getData.js
+++ api/getData.js
@@ -1,7 +1,17 @@
 export default async (req, res) => {
     const url = req.query.url;
 
-    const response = await fetch(url);
-
-    res.json(response.status);
+    const response =
+        await fetch(url)
+            .then(response => {
+                if (response.status === 200 && response.data.length > 0) {
+                    res.send({ status: response.status });
+                } else {
+                    res.send({ status: 'Bad Request' });
+                }
+            })
+            .catch(error => {
+                console.error(error);
+                res.status(400).send({ status: "Bad Request", error: error.message });
+            });
 }
